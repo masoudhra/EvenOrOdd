58f5a72b9ddabb52bd5b4a91c983c60229ff6147
Masoud Ramezani <masoudhra@gmail.com>
2023-07-09 18:12:59 +1200

    make a first project with C# and practice

---
+++ Even or Odd/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Even_or_Odd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your number");
+            string input = Console.ReadLine();
+            int number = Convert.ToInt32(input);
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("Your number is even");
+            }
+            else
+            {
+                Console.WriteLine("Your number is odd");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
